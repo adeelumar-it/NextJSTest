0f347cae6b3edda25c4b8555a5c5e010b5905b64
adeelumar <adeelumarit@gmail.com>
2024-08-04 00:05:54 -0700

    new adsense changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
 import Image from "next/image";
 import Main from "./UI/Main";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="min-h-screen ">
-      <Main></Main>
-    </main>
+    <>
+      <Head>
+        {/* Add your Google Ads script here */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9030146066299000"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (adsbygoogle = window.adsbygoogle || []).push({
+                google_ad_client: "ca-pub-9030146066299000",
+                enable_page_level_ads: true
+              });
+            `,
+          }}
+        />
+      </Head>
+      <main className="min-h-screen">
+        <Main />
+      </main>
+    </>
   );
 }
